fd4dff4c8350a5d583171918728653434a572c95
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 23:04:13 -0500

    Update layers.py

--- layers.py
+++ layers.py
@@ -37,13 +37,35 @@ class GraphAttentionLayer(nn.Module):
     Sparse version GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
 
-    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
-        super(GraphAttentionLayer, self).__init__()
+    def __init__(self, in_features, out_features, dropout, alpha, adj, concat=True):
+        super(SpGraphAttentionLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.alpha = alpha
         self.concat = concat
-
+        self.adj = adj
+        
+        edge = adj.nonzero().t()
+        self.edge = edge
+        m = torch.zeros(adj.shape[0], edge.shape[1])
+        length = []
+        count = 0
+        for i in range(edge.shape[1]):
+            if i == edge.shape[1]-1:
+                length.append(count+1)
+                break
+            elif edge[0][i] == edge[0][i+1]:
+                count = count+1
+            else:
+                count = count+1
+                length.append(count)
+                count = 0
+        j = 0
+        for i in range(len(length)):
+            m[i, range(j, j + length[i])] = 1
+            j += length[i]
+            
+        self.m = m
         self.W_high = nn.Parameter(torch.zeros(size=(in_features, out_features)))
         nn.init.xavier_normal_(self.W_high.data, gain=1.414)
         self.W_low = nn.Parameter(torch.zeros(size=(in_features, out_features)))
@@ -62,17 +84,12 @@ class GraphAttentionLayer(nn.Module):
         dv = 'cuda' if input.is_cuda else 'cpu'
 
         N = input.size()[0]
-        edge = adj.nonzero().t()
+        edge = self.edge
 
         h_high = torch.mm(input, self.W_high)
         h_low = torch.mm(input, self.W_low)
         # h: N x out
-        assert not torch.isnan(h_high).any()
-        assert not torch.isnan(h_low).any()
         
-        h_high_alike = torch.zeros_like(h_high)
-        h_low_alike = torch.zeros_like(h_low)
-
         # Self-attention on the nodes - Shared attention mechanism
         edge_h_high = torch.cat((h_high[edge[0, :], :], h_high[edge[1, :], :]), dim=1).t()
         edge_h_low = torch.cat((h_low[edge[0, :], :], h_low[edge[1, :], :]), dim=1).t()
@@ -80,8 +97,6 @@ class GraphAttentionLayer(nn.Module):
 
         edge_e_high = torch.exp(-self.leakyrelu(self.a_high.mm(edge_h_high).squeeze()))
         edge_e_low = torch.exp(-self.leakyrelu(self.a_low.mm(edge_h_low).squeeze()))
-        assert not torch.isnan(edge_e_high).any()
-        assert not torch.isnan(edge_e_low).any()
         # edge_e: E
 
         e_high_rowsum = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
@@ -96,24 +111,18 @@ class GraphAttentionLayer(nn.Module):
 
         h_high = torch.add(h_high[edge[0, :], :], h_high[edge[1, :], :])
         h_low = torch.sub(h_low[edge[0, :], :], h_low[edge[1, :], :])
+        # E x D
         
-        count = 0
-        for i in edge[0, :]:
-            h_high_alike[i] = torch.add(h_high_alike[i],h_high[edge[0, :], :][count])
-            h_low_alike[i] = torch.add(h_low_alike[i], h_low[edge[0, :], :][count])
-            count += count
+        h_high = torch.matmul(self.m, h_high)
+        h_low = torch.matmul(self.m, h_low)
             
-        h_prime_high = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), h_high_alike)
-        h_prime_low = self.special_spmm(edge, edge_e_low, torch.Size([N, N]), h_low_alike)
-        assert not torch.isnan(h_prime_high).any()
-        assert not torch.isnan(h_prime_low).any()
+        h_prime_high = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), h_high)
+        h_prime_low = self.special_spmm(edge, edge_e_low, torch.Size([N, N]), h_low)
         # h_prime: N x out
         
         h_prime_high = h_prime_high.div(e_high_rowsum)
         h_prime_low = h_prime_low.div(e_low_rowsum)
         # h_prime: N x out
-        assert not torch.isnan(h_prime_high).any()
-        assert not torch.isnan(h_prime_low).any()
 
         h_prime = 0.5 * torch.add(h_prime_high, h_prime_low)
 
