793b68f44210aa723bb5569b5bf0b7519b0c6fe7
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-02 02:59:16 -0500

    Update models.py

--- models.py
+++ models.py
@@ -25,10 +25,10 @@ class GAT(nn.Module):
                                              adj=adj,
                                              concat=False)
 
-    def forward(self, x, adj):
+    def forward(self, x):
         x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = torch.cat([att(x) for att in self.attentions], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj))
+        x = F.elu(self.out_att(x))
         return x
 
