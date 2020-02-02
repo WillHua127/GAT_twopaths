ab308ab0e56ec8a2e500e0986f1c632f4aab94aa
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 19:10:56 -0500

    Update models.py

--- models.py
+++ models.py
@@ -3,20 +3,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 from layers import GraphAttentionLayer
 
-
 class GAT(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
-        """Dense version of GAT."""
-        print("GAT initialized")
-        super(GAT, self).__init__()
+        """Sparse version of GAT."""
+        super(SpGAT, self).__init__()
         self.dropout = dropout
 
-        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
-      
+        self.attentions = [SpGraphAttentionLayer(nfeat, 
+                                                 nhid, 
+                                                 dropout=dropout, 
+                                                 alpha=alpha, 
+                                                 concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
             self.add_module('attention_{}'.format(i), attention)
 
-        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
+        self.out_att = SpGraphAttentionLayer(nhid * nheads, 
+                                             nclass, 
+                                             dropout=dropout, 
+                                             alpha=alpha, 
+                                             concat=False)
 
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
