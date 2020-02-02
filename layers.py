1f2eab47dcdfff4971d79294bd26365d178e89fa
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 19:09:45 -0500

    Update layers.py

--- layers.py
+++ layers.py
@@ -2,75 +2,127 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+    
+    
+class SpecialSpmmFunction(torch.autograd.Function):
+    """Special function for only sparse region backpropataion layer."""
+    @staticmethod
+    def forward(ctx, indices, values, shape, b):
+        assert indices.requires_grad == False
+        a = torch.sparse_coo_tensor(indices, values, shape)
+        ctx.save_for_backward(a, b)
+        ctx.N = shape[0]
+        return torch.matmul(a, b)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        a, b = ctx.saved_tensors
+        grad_values = grad_b = None
+        if ctx.needs_input_grad[1]:
+            grad_a_dense = grad_output.matmul(b.t())
+            edge_idx = a._indices()[0, :] * ctx.N + a._indices()[1, :]
+            grad_values = grad_a_dense.view(-1)[edge_idx]
+        if ctx.needs_input_grad[3]:
+            grad_b = a.t().matmul(grad_output)
+        return None, grad_values, None, grad_b
 
 
+class SpecialSpmm(nn.Module):
+    def forward(self, indices, values, shape, b):
+        return SpecialSpmmFunction.apply(indices, values, shape, b)
+
+    
 class GraphAttentionLayer(nn.Module):
     """
-    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    Sparse version GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
 
-    def __init__(self, in_features, out_features, dropout, alpha, concat=True, no_cuda=False):
-        super(GraphAttentionLayer, self).__init__()
-        self.dropout = dropout
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(SpGraphAttentionLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.alpha = alpha
         self.concat = concat
-        self.cuda = not no_cuda and torch.cuda.is_available()
 
         self.W_high = nn.Parameter(torch.zeros(size=(in_features, out_features)))
-        nn.init.xavier_uniform_(self.W_high.data, gain=1.414)
+        nn.init.xavier_normal_(self.W_high.data, gain=1.414)
         self.W_low = nn.Parameter(torch.zeros(size=(in_features, out_features)))
-        nn.init.xavier_uniform_(self.W_low.data, gain=1.414)
-        self.a_high = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.xavier_uniform_(self.a_high.data, gain=1.414)
-        self.a_low = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.xavier_uniform_(self.a_low.data, gain=1.414)
+        nn.init.xavier_normal_(self.W_low.data, gain=1.414)
+                
+        self.a_high = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
+        nn.init.xavier_normal_(self.a_high.data, gain=1.414)
+        self.a_low = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
+        nn.init.xavier_normal_(self.a_low.data, gain=1.414)
 
+        self.dropout = nn.Dropout(dropout)
         self.leakyrelu = nn.LeakyReLU(self.alpha)
+        self.special_spmm = SpecialSpmm()
 
     def forward(self, input, adj):
+        dv = 'cuda' if input.is_cuda else 'cpu'
+
+        N = input.size()[0]
+        edge = adj.nonzero().t()
+
         h_high = torch.mm(input, self.W_high)
         h_low = torch.mm(input, self.W_low)
-        h_high = F.relu6(h_high)
-        h_low = F.relu6(h_low)
+        # h: N x out
+        assert not torch.isnan(h_high).any()
+        assert not torch.isnan(h_low).any()
         
-        N = input.shape[0]
-       
-        a_input_high = torch.cat([h_high.repeat(1, N).view(N * N, -1), h_high.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
-        a_input_low = torch.cat([h_high.repeat(1, N).view(N * N, -1), h_high.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
-        
-        e_high = self.leakyrelu(torch.matmul(a_input_high, self.a_high).squeeze(2))
-        e_low = self.leakyrelu(torch.matmul(a_input_low, self.a_low).squeeze(2))
+        h_high_alike = torch.zeros_like(h_high)
+        h_low_alike = torch.zeros_like(h_low)
 
+        # Self-attention on the nodes - Shared attention mechanism
+        edge_h_high = torch.cat((h_high[edge[0, :], :], h_high[edge[1, :], :]), dim=1).t()
+        edge_h_low = torch.cat((h_low[edge[0, :], :], h_low[edge[1, :], :]), dim=1).t()
+        # edge: 2*D x E
+
+        edge_e_high = torch.exp(-self.leakyrelu(self.a_high.mm(edge_h_high).squeeze()))
+        edge_e_low = torch.exp(-self.leakyrelu(self.a_low.mm(edge_h_low).squeeze()))
+        assert not torch.isnan(edge_e_high).any()
+        assert not torch.isnan(edge_e_low).any()
+        # edge_e: E
+
+        e_high_rowsum = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
+        e_low_rowsum = self.special_spmm(edge, edge_e_low, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
+        # e_rowsum: N x 1
+
+        edge_e_high = F.relu6(edge_e_high)
+        edge_e_high = self.dropout(edge_e_high)
+        edge_e_low = F.relu6(edge_e_low)
+        edge_e_low = self.dropout(edge_e_low)
+        # edge_e: E
+
+        h_high = torch.add(h_high[edge[0, :], :], h_high[edge[1, :], :])
+        h_low = torch.sub(h_low[edge[0, :], :], h_low[edge[1, :], :])
         
-        adj_unnormalized = torch.zeros_like(adj)
-        one = torch.ones((1,1))
-        if self.cuda:
-            adj_unnormalized = adj_unnormalized.cuda()
-            one = one.cuda()
-        adj_unnormalized = torch.where(adj > 0, one, adj_unnormalized)
-        attention_high = torch.matmul(e_high, adj_unnormalized)
-        attention_low = torch.matmul(e_low, adj_unnormalized)
-        attention_high = F.relu6(attention_high)
-        attention_high = F.softmax(attention_high, dim=1)
-        attention_low = F.relu6(attention_low)
-        attention_low = F.softmax(attention_low, dim=1)
+        count = 0
+        for i in edge[0, :]:
+            h_high_alike[i] = torch.add(h_high_alike[i],h_high[edge[0, :], :][count])
+            h_low_alike[i] = torch.add(h_low_alike[i], h_low[edge[0, :], :][count])
+            count += count
+            
+        h_prime_high = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), h_high_alike)
+        h_prime_low = self.special_spmm(edge, edge_e_low, torch.Size([N, N]), h_low_alike)
+        assert not torch.isnan(h_prime_high).any()
+        assert not torch.isnan(h_prime_low).any()
+        # h_prime: N x out
         
-        attention_high = F.dropout(attention_high, self.dropout, training=self.training)
-        attention_low = F.dropout(attention_low, self.dropout, training=self.training)
-        
-        h_high = torch.add(h_high.repeat(1, N).view(N * N, -1), h_high.repeat(N, 1)).view(N, -1, self.out_features)
-        h_high = torch.sum(h_high, 0)
-        h_low = torch.sub(h_low.repeat(1, N).view(N * N, -1), h_low.repeat(N, 1)).view(N, -1, self.out_features) 
-        h_low = torch.sum(h_low, 0)
-        
-        h_high = torch.matmul(attention_high, h_high)
-        h_low = torch.matmul(attention_low, h_low)
-        h_prime = 0.5 * torch.add(h_high, h_low)
-        h_prime = F.relu6(h_prime)
-        
-        return h_prime
+        h_prime_high = h_prime_high.div(e_high_rowsum)
+        h_prime_low = h_prime_low.div(e_low_rowsum)
+        # h_prime: N x out
+        assert not torch.isnan(h_prime_high).any()
+        assert not torch.isnan(h_prime_low).any()
+
+        h_prime = 0.5 * torch.add(h_prime_high, h_prime_low)
+
+        if self.concat:
+            # if this layer is not last layer,
+            return F.elu(h_prime)
+        else:
+            # if this layer is last layer,
+            return h_prime
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
