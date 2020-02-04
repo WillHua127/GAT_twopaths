3d82b5b86e215bbf35b97201cad7776cbaeda5a4
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-03 22:49:11 -0500

    Add files via upload

--- layers.py
+++ layers.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import scipy.sparse as sp
     
 class SpecialSpmmFunction(torch.autograd.Function):
     """Special function for only sparse region backpropataion layer."""
@@ -31,48 +30,22 @@ class SpecialSpmm(nn.Module):
     def forward(self, indices, values, shape, b):
         return SpecialSpmmFunction.apply(indices, values, shape, b)
 
-def sparse_mx_to_torch_sparse_tensor(sparse_mx):
-        """Convert a scipy sparse matrix to a torch sparse tensor."""
-        sparse_mx = sparse_mx.tocoo().astype(np.float32)
-        indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
-        values = torch.from_numpy(sparse_mx.data)
-        shape = torch.Size(sparse_mx.shape)
-        return torch.sparse.FloatTensor(indices, values, shape)
 
 class GraphAttentionLayer(nn.Module):
     """
     Sparse version GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
 
-    def __init__(self, in_features, out_features, dropout, alpha, adj, concat=True, no_cuda=False):
+    def __init__(self, in_features, out_features, dropout, alpha, adj, dataset, edge, m, concat=True, no_cuda=False):
         super(GraphAttentionLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.alpha = alpha
         self.concat = concat
         self.adj = adj
-        
-        edge = adj._indices()
+        self.dataset = dataset
         self.edge = edge
-        m = sp.lil_matrix((adj.shape[0], edge.shape[1]))
-        length = []
-        count = 0
-        for i in range(edge.shape[1]):
-            if i == edge.shape[1]-1:
-                length.append(count+1)
-                break
-            elif edge[0][i] == edge[0][i+1]:
-                count = count+1
-            else:
-                count = count+1
-                length.append(count)
-                count = 0
-        j = 0
-        for i in range(len(length)):
-            m[i,range(j, j + length[i])] = 1
-            j += length[i]
-            
-        self.m = sparse_mx_to_torch_sparse_tensor(m)
+        self.m = m
         
         cuda = not no_cuda and torch.cuda.is_available()
         if cuda:
@@ -83,37 +56,83 @@ class GraphAttentionLayer(nn.Module):
         self.W_low = nn.Parameter(torch.zeros(size=(in_features, out_features)))
         nn.init.xavier_normal_(self.W_low.data, gain=1.414)
                 
-        self.a_high = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
+        self.a_high = nn.Parameter(torch.zeros(size=(1, 4*out_features)))
         nn.init.xavier_normal_(self.a_high.data, gain=1.414)
-        self.a_low = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
+        self.a_low = nn.Parameter(torch.zeros(size=(1, 4*out_features)))
         nn.init.xavier_normal_(self.a_low.data, gain=1.414)
+        
+        #self.c_high = nn.Parameter(torch.zeros(size=(1, 1)))
+        #nn.init.xavier_uniform_(self.c_high.data, gain=1)
+        #self.c_low = nn.Parameter(torch.zeros(size=(1, 1)))
+        #nn.init.xavier_uniform_(self.c_low.data, gain=1)
+
+        self.g_high = nn.Parameter(torch.zeros(size=(1, 1)))
+        nn.init.xavier_uniform_(self.g_high.data, gain=1)
+        self.b_high = nn.Parameter(torch.zeros(size=(1, 1)))
+        nn.init.xavier_uniform_(self.b_high.data, gain=1)
+        self.g_low = nn.Parameter(torch.zeros(size=(1, 1)))
+        nn.init.xavier_uniform_(self.g_low.data, gain=1)
+        self.b_low = nn.Parameter(torch.zeros(size=(1, 1)))
+        nn.init.xavier_uniform_(self.b_low.data, gain=1)
 
         self.dropout = nn.Dropout(dropout)
         self.leakyrelu = nn.LeakyReLU(self.alpha)
         self.special_spmm = SpecialSpmm()
 
+    def gam(self, x, epsilon=1e-6):
+        return F.relu6(x+3)/3 + epsilon
+
+    def relu_bt(self, x):
+        threshold = torch.norm(x,p=float("inf")).clone().detach()#(x.shape[0] * x.shape[1])#torch.Tensor([1e-6]).cuda()#torch.norm(x,p=float("inf")).clone().detach()#/(x.shape[0] * x.shape[1]) # p=1,2,float("inf"), torch.Tensor([1e-6]).cuda()
+        #print(" threshold: ", threshold)
+        return - torch.threshold(-F.leaky_relu(x),-threshold,-threshold) #F.relu(x-threshold) #- torch.threshold(-F.relu(x),-threshold,-threshold)
+        #return F.relu6(x)
+        #return F.relu(x-threshold)+threshold
+        #return torch.exp(F.relu(x-threshold)+threshold)-torch.Tensor([1])
+        #return torch.tanh(F.relu(x-threshold)) #
+        #return torch.nn.functional.gelu(x)
+        #return F.elu(x)
+        #return F.relu(x)
     
     def forward(self, input):
         dv = 'cuda' if input.is_cuda else 'cpu'
 
-        N = self.adj.size()[0]
+        if self.dataset == 'citeseer':
+            N = self.adj.size()[0]
+        else:
+            N = input.size()[0]
         edge = self.edge
+
+        gamma_high = self.gam(self.g_high)
+        beta_high = self.gam(self.b_high)
+        gamma_low = self.gam(self.g_low)
+        beta_low = self.gam(self.b_low)
+        #c_high = self.gam(self.c_high)
+        #c_low = self.gam(self.c_low) - 1
 
         h_high = torch.mm(input, self.W_high)
         h_low = torch.mm(input, self.W_low)
-        h_high = self.leakyrelu(h_high)
-        h_low = self.leakyrelu(h_low)
+        h_high = self.relu_bt(h_high)
+        h_low = self.relu_bt(h_low)
         # h: N x out
         assert not torch.isnan(h_high).any()
+
+        input1 = torch.add((h_high[edge[0, :], :]), h_high[edge[1, :], :])
+        input2 = torch.sub(h_low[edge[0, :], :], h_low[edge[1, :], :])
         
         # Self-attention on the nodes - Shared attention mechanism
-        edge_h_high = torch.cat((h_high[edge[0, :], :], h_high[edge[1, :], :]), dim=1).t()
-        edge_h_low = torch.cat((h_low[edge[0, :], :], h_low[edge[1, :], :]), dim=1).t()
+        edge_h_high = torch.cat((beta_high*h_high[edge[0, :], :], (2-beta_high)*h_high[edge[1, :], :], gamma_high*input1, (2-gamma_high)*input2), dim=1).t()
+        edge_h_low = torch.cat((beta_low*h_low[edge[0, :], :], (2-beta_low)*h_low[edge[1, :], :], gamma_low*input1, (2-gamma_low)*input2), dim=1).t()
         assert not torch.isnan(edge_h_high).any()
         # edge: 2*D x E
 
-        edge_e_high = torch.exp(-self.leakyrelu(self.a_high.mm(edge_h_high).squeeze()))
-        edge_e_low = torch.exp(-self.leakyrelu(self.a_low.mm(edge_h_low).squeeze()))
+        edge_e_high = torch.exp(-self.leakyrelu(torch.div(self.a_high.mm(edge_h_high).squeeze(), torch.norm(self.a_high))))
+        edge_e_low = torch.exp(-self.leakyrelu(torch.div(self.a_low.mm(edge_h_low).squeeze(), torch.norm(self.a_low))))
+        assert not torch.isnan(edge_e_high).any()
+        # edge_e: E
+
+        edge_e_high = self.dropout(edge_e_high)
+        edge_e_low = self.dropout(edge_e_low)
         assert not torch.isnan(edge_e_high).any()
         # edge_e: E
 
@@ -122,18 +141,9 @@ class GraphAttentionLayer(nn.Module):
         assert not torch.isnan(e_high_rowsum).any()
         # e_rowsum: N x 1
 
-        edge_e_high = self.dropout(edge_e_high)
-        edge_e_low = self.dropout(edge_e_low)
-        assert not torch.isnan(edge_e_high).any()
-        # edge_e: E
-
-        h_high = torch.add(h_high[edge[0, :], :], h_high[edge[1, :], :])
-        h_low = torch.sub(h_low[edge[0, :], :], h_low[edge[1, :], :])
-        assert not torch.isnan(h_high).any()
-        # E x D
         
-        h_high = torch.matmul(self.m, h_high)
-        h_low = torch.matmul(self.m, h_low)
+        h_high = torch.matmul(self.m, input1)
+        h_low = torch.matmul(self.m, input2)
         assert not torch.isnan(h_high).any()
             
         h_prime_high = self.special_spmm(edge, edge_e_high, torch.Size([N, N]), h_high)
@@ -141,20 +151,23 @@ class GraphAttentionLayer(nn.Module):
         assert not torch.isnan(h_prime_high).any()
         # h_prime: N x out
         
-        h_prime_high = h_prime_high.div(e_high_rowsum+1e-9)
-        h_prime_low = h_prime_low.div(e_low_rowsum+1e-9)
+        h_prime_high = h_prime_high.div(e_high_rowsum+1e-16)
+        h_prime_low = h_prime_low.div(e_low_rowsum+1e-16)
         assert not torch.isnan(h_prime_high).any()
         # h_prime: N x out
 
-        h_prime = 0.5 * torch.add(h_prime_high, h_prime_low)
+        #h_prime_high = c_high * h_prime_high
+        #h_prime_low = c_low * h_prime_low
+
+        h_prime = torch.add(h_prime_high, h_prime_low)
         assert not torch.isnan(h_prime).any()
 
         if self.concat:
             # if this layer is not last layer,
-            return F.elu(h_prime)
+            return self.relu_bt(h_prime)
         else:
             # if this layer is last layer,
-            return h_prime
+            return self.relu_bt(h_prime)
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
--- models.py
+++ models.py
@@ -2,18 +2,52 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers import GraphAttentionLayer
+import scipy.sparse as sp
+import numpy as np
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+        """Convert a scipy sparse matrix to a torch sparse tensor."""
+        sparse_mx = sparse_mx.tocoo().astype(np.float32)
+        indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+        values = torch.from_numpy(sparse_mx.data)
+        shape = torch.Size(sparse_mx.shape)
+        return torch.sparse.FloatTensor(indices, values, shape)
 
 class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, adj):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads, adj, dataset):
         """Sparse version of GAT."""
         super(GAT, self).__init__()
         self.dropout = dropout
+        edge = adj._indices()
+
+        m = sp.lil_matrix((adj.shape[0], edge.shape[1]))
+        length = []
+        count = 0
+        for i in range(edge.shape[1]):
+            if i == edge.shape[1]-1:
+                length.append(count+1)
+                break
+            elif edge[0][i] == edge[0][i+1]:
+                count = count+1
+            else:
+                count = count+1
+                length.append(count)
+                count = 0
+        j = 0
+        for i in range(len(length)):
+            m[i,range(j, j + length[i])] = 1
+            j += length[i]
+            
+        m = sparse_mx_to_torch_sparse_tensor(m)
 
         self.attentions = [GraphAttentionLayer(nfeat, 
                                                  nhid, 
                                                  dropout=dropout, 
                                                  alpha=alpha,
                                                  adj=adj,
+                                                 dataset=dataset,
+                                                 edge=edge,
+                                                 m=m,
                                                  concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
             self.add_module('attention_{}'.format(i), attention)
@@ -23,12 +57,15 @@ class GAT(nn.Module):
                                              dropout=dropout, 
                                              alpha=alpha,
                                              adj=adj,
+                                             dataset=dataset,
+                                             edge=edge,
+                                             m=m,
                                              concat=False)
 
     def forward(self, x):
         x = F.dropout(x, self.dropout, training=self.training)
         x = torch.cat([att(x) for att in self.attentions], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x))
+        x = self.out_att(x)
         return x
 
